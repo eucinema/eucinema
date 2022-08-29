55e4ddda374555ddf7bc0e67bf508ebaa45b3172
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2022-08-29 08:20:27 -0300

    salvar dados de login para verificação
    Agora, só falta acrescentar a procura no mysql para validar o login

--- scripts/pop-up-login.js
+++ scripts/pop-up-login.js
@@ -5,3 +5,9 @@ function openForm() {
 function closeForm() {
     document.getElementById("login").style.display = "none";
 }
+
+function login() {
+    let email = document.getElementById("email").value; 
+    let psw = document.getElementById("psw").value; 
+    console.log(email, psw);
+}
